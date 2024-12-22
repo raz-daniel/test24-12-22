b865bd910599b829728d8187df22503ca758641a
raz-daniel <raz.daniel@gmail.com>
2024-12-22 10:47:24 +0200

    finish question 1 after adding region stats in a table

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,60 +2,81 @@
 
 (() => {
 
-const collectData = url => fetch(url).then(response => response.json())
+    const collectData = url => fetch(url).then(response => response.json())
 
-const generateCountriesHTML = countries => {
-    const newCountriesHTML = countries.map(country => {
-        const {name: {common}, population} = country
-        return `
+    const generateCountriesHTML = countries => {
+        const newCountriesHTML = countries.map(country => {
+            const { name: { common }, population } = country
+            return `
         <tr>
                 <td>${common}</td>
                 <td>${population}</td>
             </tr>
         `
-    }).join('')
-    return newCountriesHTML;
-}
+        }).join('')
+        return newCountriesHTML;
+    }
 
-const generateCountriesStatsHTML = countries => {
-    const numberOfCountries = countries.length
-    const numberOfCitizensInCountries = countries.reduce((sum, country) => {
-        const {population} = country
-        return sum+population
-    },0)
-    const averageCitizens = (numberOfCitizensInCountries / numberOfCountries).toFixed(2)
-    return `
+    const generateCountriesStatsHTML = countries => {
+        const numberOfCountries = countries.length
+        const numberOfCitizensInCountries = countries.reduce((sum, country) => {
+            const { population } = country
+            return sum + population
+        }, 0)
+        const averageCitizens = (numberOfCitizensInCountries / numberOfCountries).toFixed(2)
+        return `
     <li>Num of Countries: ${numberOfCountries}</li>
     <li>num of world citizens ${numberOfCitizensInCountries}</li>
     <li>average of citizens is: ${averageCitizens}</li>
     `
-}
+    }
 
-const renderCountriesHTML = newCountriesHTML => document.getElementById('countriesTBody').innerHTML = newCountriesHTML
+    const generateRegionsHTML = countries => {
+        const uniqueRegions = [...new Set(countries.map(country => {
+            const { region } = country
+            return region
+        }))]
+            .map(region => ({
+                region,
+                count: countries.filter(country => country.region === region).length
+            }))
+            .map(area => {
+                const { region, count } = area
+                return `
+                 <tr>
+                     <td>${region}</td>
+                    <td>${count}</td>
+                 </tr>
+        `
+            }).join('')
+        return uniqueRegions;
+    }
 
-const renderCountriesStatsHTML = newCountriesStats => document.getElementById('basicUlStats').innerHTML = newCountriesStats
+    const renderCountriesHTML = newCountriesHTML => document.getElementById('countriesTBody').innerHTML = newCountriesHTML
+
+    const renderCountriesStatsHTML = newCountriesStats => document.getElementById('basicUlStats').innerHTML = newCountriesStats
+
+    const renderRegionsHTML = regionsHTML => document.getElementById('regionTBody').innerHTML = regionsHTML
 
 
-{/* <tr>
-                <td>${region}</td>
-                <td>${name.common}</td>
-            </tr> */}
 
 
-document.getElementById('countriesForm').addEventListener('submit', async (event) => {
-    event.preventDefault();
-    
-    const countriesData = await collectData('https://restcountries.com/v3.1/all');
-    const countriesHTML = generateCountriesHTML(countriesData)
-    const countriesStats = generateCountriesStatsHTML(countriesData)
-    renderCountriesHTML(countriesHTML)
-    renderCountriesStatsHTML(countriesStats)
+    document.getElementById('countriesForm').addEventListener('submit', async (event) => {
+        event.preventDefault();
 
-})
+        const countriesData = await collectData('https://restcountries.com/v3.1/all');
+        const countriesHTML = generateCountriesHTML(countriesData)
+        const countriesStats = generateCountriesStatsHTML(countriesData)
+        const regionsHTML = generateRegionsHTML(countriesData)
+        renderCountriesHTML(countriesHTML)
+        renderCountriesStatsHTML(countriesStats)
+        renderRegionsHTML(regionsHTML)
 
-document.getElementById('showNameData').addEventListener('click', (event) => {
-    event.preventDefault()
-})
+    })
+
+    document.getElementById('showNameData').addEventListener('click', (event) => {
+        event.preventDefault()
+    })
 
 
 })()
